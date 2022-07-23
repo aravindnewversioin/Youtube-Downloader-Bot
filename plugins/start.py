f56ae8063099bd040161544ccccb70e2e07bb80b
Aravind <93819841+aravindnewversioin@users.noreply.github.com>
2022-07-23 22:58:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,7 +5,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aravind_bots")],
+        [InlineKeyboardButton("Channel", url="https://t.me/aravindbots")],
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/aravinddnivara")]
     ])
